934e97f1c7388fe176505e7da05b69e1ced2aac1
SIMONE MUSSINI <284900@studenti.unimore.it>
2026-03-16 15:19:55 +0100

    Fixed .bat scrpit for starting powershell usb detection script from the company laptop that has trouble with UNC pathing

--- start_acc.bat
+++ start_acc.bat
@@ -1,2 +1,5 @@
 @echo off
-PowerShell.exe -ExecutionPolicy Bypass -File "%~dp0start_acc.ps1"
+copy "%~dp0start_acc.ps1" "%TEMP%\start_acc.ps1" /Y >nul
+PowerShell.exe -ExecutionPolicy Bypass -File "%TEMP%\start_acc.ps1"
+timeout /t 3 /nobreak >nul
+del "%TEMP%\start_acc.ps1" >nul 2>&1
